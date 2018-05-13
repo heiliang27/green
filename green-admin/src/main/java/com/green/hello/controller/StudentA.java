1a40d8ad1413372e8b76f29cfa4682aa527ce005
limingliang <limingliang@limingliang-PC>
2018-05-13 21:08:08 +0800

    引入公共模块

--- green-admin/src/main/java/com/green/hello/controller/StudentA.java
+++ green-admin/src/main/java/com/green/hello/controller/StudentA.java
@@ -6,12 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.green.common.util.StringUtils;
+
 @Component
 public class StudentA {
 	@Autowired
 	StudentB studentB;
 	
-	public void sortList(List<String> list){
+	public void sortList(List<String> list){     
+		StringUtils.isEmpty("");
 		 Collections.sort(list);  
 	}
 
