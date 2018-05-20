1ef1b3aaef8277ac203724d6008b13eb6fbd91df
limingliang <limingliang@limingliang-PC>
2018-05-20 22:18:26 +0800

    web配置类

---
+++ green-admin/src/main/java/com/green/common/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.green.common.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class WebConfig implements WebMvcConfigurer {
+	// 初始化mvc配置类
+}
